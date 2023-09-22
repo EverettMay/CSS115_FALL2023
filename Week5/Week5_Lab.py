f02a28056ed9915f0747238817ac5fc63ee50c43
EverettMay <145547081+EverettMay@users.noreply.github.com>
2023-09-22 10:43:42 -0400

    1

---
+++ Week5/Week5_Lab.py
@@ -0,0 +1,17 @@
+# firstNumber = float(input("First number:"))
+# secondNumber = float(input("Second number:"))
+
+# if (firstNumber > secondNumber):
+# 	print(firstNumber, '>', secondNumber)
+# elif (firstNumber, '<', secondNumber):
+# 	print(firstNumber, '<', secondNumber)
+# else:
+# 	print(firstNumber, '=', secondNumber)
+
+numberCredit = int(input("Enter the number of credit you completed:"))
+
+if (numberCredit < 120):
+	print("You are not eligible")
+
+else:
+	GPA = float(input("Enter GPA:"))
